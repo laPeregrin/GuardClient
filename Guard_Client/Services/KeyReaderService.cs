196abf7724166e0f05bf4a4be9772f44eb857da6
Vladimir <64407348+laPeregrin@users.noreply.github.com>
2021-01-23 18:07:25 +0200

    add SerialPort

---
+++ Guard_Client/Services/KeyReaderService.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO.Ports;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Guard_Client.Services
+{
+    public class KeyReaderService
+    {
+        public SerialPort serialPort {get;}
+        public KeyReaderService()
+        {
+            serialPort = new SerialPort()
+            {
+                PortName = "COM1",
+                BaudRate = 115200,
+                Parity = Parity.None,
+                StopBits = StopBits.One,
+                DataBits = 7,
+                Handshake = Handshake.None,
+                Encoding = ASCIIEncoding.ASCII
+            };
+        }
+    }
+}
--- Guard_Client/obj/Debug/netcoreapp3.1/App.g.i.cs
+++ Guard_Client/obj/Debug/netcoreapp3.1/App.g.i.cs
@@ -48,7 +48,7 @@ namespace Guard_Client {
         /// InitializeComponent
         /// </summary>
         [System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "5.0.1.0")]
+        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "5.0.2.0")]
         public void InitializeComponent() {
             if (_contentLoaded) {
                 return;
@@ -74,7 +74,7 @@ namespace Guard_Client {
         /// </summary>
         [System.STAThreadAttribute()]
         [System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "5.0.1.0")]
+        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "5.0.2.0")]
         public static void Main() {
             Guard_Client.App app = new Guard_Client.App();
             app.InitializeComponent();
