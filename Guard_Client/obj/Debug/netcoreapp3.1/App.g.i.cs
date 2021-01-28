6f11d1eaa8f6d5fe5f565f46a97641bb636b9314
Vladimir <64407348+laPeregrin@users.noreply.github.com>
2021-01-28 11:58:27 +0200

    0.9

--- Guard_Client/obj/Debug/netcoreapp3.1/App.g.i.cs
+++ Guard_Client/obj/Debug/netcoreapp3.1/App.g.i.cs
@@ -60,7 +60,7 @@ namespace Guard_Client {
             
             #line default
             #line hidden
-            System.Uri resourceLocater = new System.Uri("/Guard_Client;component/app.xaml", System.UriKind.Relative);
+            System.Uri resourceLocater = new System.Uri("/Guard_Client;V1.0.0.0;component/app.xaml", System.UriKind.Relative);
             
             #line 1 "..\..\..\App.xaml"
             System.Windows.Application.LoadComponent(this, resourceLocater);
--- Guard_Client/obj/Debug/netcoreapp3.1/Views/Pages/GeneralPage.g.i.cs
+++ Guard_Client/obj/Debug/netcoreapp3.1/Views/Pages/GeneralPage.g.i.cs
@@ -54,7 +54,7 @@ namespace Guard_Client.Views.Pages {
                 return;
             }
             _contentLoaded = true;
-            System.Uri resourceLocater = new System.Uri("/Guard_Client;component/views/pages/generalpage.xaml", System.UriKind.Relative);
+            System.Uri resourceLocater = new System.Uri("/Guard_Client;V1.0.0.0;component/views/pages/generalpage.xaml", System.UriKind.Relative);
             
             #line 1 "..\..\..\..\..\Views\Pages\GeneralPage.xaml"
             System.Windows.Application.LoadComponent(this, resourceLocater);
